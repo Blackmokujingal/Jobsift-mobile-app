64bdc787af4f2dff3bff3782d1f734fbacd15f84
Beatrice Washington <bea800.w98@gmail.com>
2023-05-03 14:29:45 +0400

    Linked the API to the NearbyJob section

---
+++ app/job-details/[id].js
@@ -0,0 +1,11 @@
+import { Text, View, SafeAreaView, ScrollView, ActivityIndicator } from 'react-native'
+
+const JobDetails = () => {
+  return (
+    <Text>
+      JobDetails
+    </Text>
+  )
+}
+
+export default JobDetails
